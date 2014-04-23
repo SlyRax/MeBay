4ce5d7790761d4455d449f61be88f38ce1348b8c
Will <will.manning@gmail.com>
2014-04-23 19:02:21 +0100

    Adding the create functionality

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,8 +1,16 @@
 class AdsController < ApplicationController
+  def new
+    @ad = Ad.new
+  end
+  def create 
+    @ad = Ad.new(params[:ad])
+    @ad.save
+  end
 	def show
 		@ad = Ad.find(params[:id])
 	end
 	def index
 		@ads = Ad.find(:all)
 	end
+	
 end
