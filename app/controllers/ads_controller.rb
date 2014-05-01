8ed4dfaf3dab1543a77ca2a5b9301e489b1d95cc
Will <will.manning@gmail.com>
2014-05-01 21:45:35 +0100

    Adding security

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,4 +1,5 @@
 class AdsController < ApplicationController
+  before_filter :check_logged_in, :only => [:edit, :update]
   def new
     @ad = Ad.new
   end
@@ -24,6 +25,11 @@ class AdsController < ApplicationController
 	private 
 	 def ad_params
 	     params.require(:ad).permit(:name, :description, :price, :seller_id, :email, :img_url)
+	 end
+	 def check_logged_in
+	   authenticate_or_request_with_http_basic("Ads") do |username, password|
+	     username == "admin" && password == "password"
+	   end
 	 end
 	
 end
