dfc947d81a4dfa5b7ec1f3db959cb075674f08a8
Will <will.manning@gmail.com>
2014-05-01 21:22:46 +0100

    Updating edit

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -13,7 +13,14 @@ class AdsController < ApplicationController
 	def index
 		@ads = Ad.find(:all)
 	end
-	
+	def edit
+	  @ad = Ad.find(params[:id])
+	end
+	def update
+	  @ad = Ad.find(params[:id])
+	  @ad.update_attributes(ad_params)
+	  redirect_to "/ads/#{@ad.id}"
+	end
 	private 
 	 def ad_params
 	     params.require(:ad).permit(:name, :description, :price, :seller_id, :email, :img_url)
